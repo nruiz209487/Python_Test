f957c4462cd74ac2d269621b519c19e11a9afb6d
Neos Rafael Ruiz Sanchez <neos.ruiz@iesnervion.es>
2024-11-12 12:38:40 +0100

    qq

--- JuegoDelParchis/parchis.py
+++ JuegoDelParchis/parchis.py
@@ -1,61 +1,50 @@
 import random
 
 class Parchis:
-    TAM_TABLERO =20 
-    def __init__(self,nomJ1,nomJ2):
-
-        self.fichaJ1 =0
-        self.fichaJ2 =0
-        self.dado1 = 0
-        self.dado2 = 0
-        self.nomJ1 =nomJ1
-        self.nomJ2 =nomJ2
-
-    @staticmethod
-    def tirarDados(self):
-        self.dado1 = random.randint(1, 6)
-        self.dado2 = random.randint(1, 6)
-        return  self.dado1+ self.dado2
+    TAM_TABLERO = 20
+    dado1 = 0
+    dado2 = 0
     
-    @staticmethod
+    def __init__(self, nomJ1, nomJ2):
+        self.fichaJ1 = 0
+        self.fichaJ2 = 0
+        self.nomJ1 = nomJ1
+        self.nomJ2 = nomJ2
+
+    def tirarDados(self):
+        Parchis.dado1 = random.randint(1, 6)
+        Parchis.dado2 = random.randint(1, 6)
+        return Parchis.dado1 + Parchis.dado2
+
     def pintaTablero(self):
-        res="/t"
-        for i in range(0,self.TAM_TABLERO+1):
-            res+=i + " "
-        res+="F"
+        res = ""
         return res
 
+    def avanzaPosiciones(self, nombreJugador):
+        if nombreJugador == self.nomJ1:
+            self.fichaJ1 += self.tirarDados()
+            if self.fichaJ1 > Parchis.TAM_TABLERO:
+                casillasPasadas = self.fichaJ1 - Parchis.TAM_TABLERO
+                self.fichaJ1 = Parchis.TAM_TABLERO - casillasPasadas
+        if nombreJugador == self.nomJ2:
+            self.fichaJ2 += self.tirarDados()
+            if self.fichaJ2 > self.TAM_TABLERO:
+                casillasPasadas = self.fichaJ2 - Parchis.TAM_TABLERO
+                self.fichaJ2 = Parchis.TAM_TABLERO - casillasPasadas
 
-    @staticmethod
-    def avanzaPosiciones(self,nombreJugador):
-        if(nombreJugador== self.nomJ1):
-            Parchis.fichaJ1= Parchis.tirarDados(self)
-            if(Parchis.fichaJ1> self.TAM_TABLERO):
-                casillasPasadas=Parchis.fichaJ1-self.TAM_TABLERO
-                Parchis.fichaJ1=self.TAM_TABLERO
-                Parchis.fichaJ1= Parchis.fichaJ1-casillasPasadas
-        if(nombreJugador== self.nomJ2):
-            Parchis.fichaJ2= Parchis.tirarDados(self)
-            if(Parchis.fichaJ2> self.TAM_TABLERO):
-                casillasPasadas=Parchis.fichaJ2-self.TAM_TABLERO
-                Parchis.fichaJ2=self.TAM_TABLERO
-                Parchis.fichaJ2= Parchis.fichaJ2-casillasPasadas
-
-    @staticmethod
     def estadoCarrera(self):
-        res=""
-        if(Parchis.fichaJ1 > Parchis.fichaJ2):
-            res="Va ganando el jugador 1 "
-        elif(Parchis.fichaJ1 < Parchis.fichaJ2):
-            res="Va ganando el jugador 2 "
+        if self.fichaJ1 > self.fichaJ2:
+            return "Va ganando el jugador 1"
+        elif self.fichaJ1 < self.fichaJ2:
+            return "Va ganando el jugador 2"
         else:
-            res="Empate"
+            return "Empate"
 
-
-    @staticmethod
     def esGanador(self):
-        res=""
-        return res
-       
-
+        if self.fichaJ1 == Parchis.TAM_TABLERO:
+            return f"El ganador es {self.nomJ1}"
+        elif self.fichaJ2 == Parchis.TAM_TABLERO:
+            return f"El ganador es {self.nomJ2}"
+        else:
+            return "Aún no hay ganador"
 
