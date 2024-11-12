23ba37387685072d3494a05bc2d84006641f5779
Neos Rafael Ruiz Sanchez <neos.ruiz@iesnervion.es>
2024-11-12 12:41:36 +0100

    aa

--- JuegoDelParchis/Test/test_tirarDados.py
+++ JuegoDelParchis/Test/test_tirarDados.py
@@ -2,5 +2,5 @@ from JuegoDelParchis.Parchis import *
 
 def test_tirarDados():
     objetoParchis =  Parchis("Jug1","Jug2")
-    assert objetoParchis.tirarDados()==5
+    assert objetoParchis.tirarDados()== 1 or 2 or 3 or 4 or 5 or 6
 
