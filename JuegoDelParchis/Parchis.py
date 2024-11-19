3284d7df6014b2ec8031f548303710768b3ed655
Neos Rafael Ruiz Sanchez <neos.ruiz@iesnervion.es>
2024-11-19 12:26:24 +0100

    aa

--- JuegoDelParchis/Parchis.py
+++ JuegoDelParchis/Parchis.py
@@ -64,10 +64,11 @@ class Parchis:
     def esGanador(self):
         res=""
         if self.fichaJ1 == Parchis.TAM_TABLERO:
-            res=  "El ganador es " + self.nomJ1
+            res=  + self.nomJ1
         elif self.fichaJ2 == Parchis.TAM_TABLERO:
-            res= "El ganador es "+ self.nomJ2
+            res=  self.nomJ2
         else:
-            res= "Empate"
+             res=""
+
         return res
 
--- JuegoDelParchis/Test/test_esGanador.py
+++ JuegoDelParchis/Test/test_esGanador.py
@@ -1,11 +1,5 @@
 from JuegoDelParchis.Parchis import *
 
-def test_esGanadorEmpate():
-    objetoParchis = Parchis("Jug1", "Jug2")
-    objetoParchis.fichaJ1 = 10
-    objetoParchis.fichaJ2 = 15
-    resultado = objetoParchis.esGanador()
-    assert resultado == "Empate"
 
 def test_esGanadorJug1():
     objetoParchis = Parchis("Jug1", "Jug2")
@@ -20,3 +14,10 @@ def test_esGanadorJug2():
     objetoParchis.fichaJ2 = Parchis.TAM_TABLERO
     resultado = objetoParchis.esGanador()
     assert resultado == "El ganador es Jug2"
+
+def test_noHayGanador():
+    objetoParchis = Parchis("Jug1", "Jug2")
+    objetoParchis.fichaJ1 = 0
+    objetoParchis.fichaJ2 = 0
+    resultado = objetoParchis.esGanador()
+    assert resultado == ""
